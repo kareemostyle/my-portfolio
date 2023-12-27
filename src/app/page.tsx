f57e7e5b5ba882c29716c0272c7c1baf3c42a01f
Kareem Taleb <43004711+kareemostyle@users.noreply.github.com>
2023-12-27 15:26:31 +0000

    Initial portfolio commit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Portfolio from "./portfolio/page";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Portfolio/>
+    </main>
   );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,29 @@
 const Portfolio = () => {
-  return <></>;
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
+      {/* Hero Section */}
+      <div className="text-center">
+        {/* Background Image or Video */}
+        {/* Customize the path based on your project structure */}
+        <img
+          src="/path/to/your/background-image.jpg"
+          alt="Background"
+          className="w-full h-auto mb-8 rounded-lg shadow-lg"
+        />
+
+        {/* Headline */}
+        <h1 className="text-4xl font-bold mb-4">Coding, Gaming, Hooping</h1>
+
+        {/* Subheading or Tagline */}
+        <p className="text-lg mb-8">A Developer's Journey</p>
+
+        {/* CTA Button */}
+        <a href="/portfolio" className="btn">
+          Explore My Work
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
